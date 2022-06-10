7fd492465f618d2736750392470ba225740056e8
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-06-10 14:57:32 +0200

    edits

---
+++ src/js/user-register.js
@@ -0,0 +1,42 @@
+/* ----------------------------------------------------
+    Setup
+---------------------------------------------------- */
+
+/* --- Tab References --- */
+
+    // Get reference to all elements in register tab
+    const registerTab = document.getElementsByClassName("register");
+    console.log(registerTab);
+
+    // Get reference to all elements in userList tab
+    const userTabs = document.getElementsByClassName("users");
+    console.log(userTabs);
+
+
+/* --- Nav references --- */
+
+    // code here...
+
+
+/* --- Form field references --- */
+
+    // code here...
+
+
+/* --- Table list reference --- */
+
+    // code here...
+
+
+
+/* ----------------------------------------------------
+    Functionality
+---------------------------------------------------- */
+
+    // code here...
+
+
+
+/* ----------------------------------------------------
+    Event Listenters and Interactivity
+---------------------------------------------------- */
